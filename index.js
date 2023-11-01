8d1b1327b86edfe5467d2a03761aac97a5d3719b
rous <rous@gmail.com>
2023-11-01 12:32:44 -0500

    punto y coma

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 const PORT = 3000;
 
-app.listen(PORT, ()=>console.log(`servidor corriendo en puerto ${PORT}`))
+app.listen(PORT, ()=>console.log(`servidor corriendo en puerto ${PORT}`));
 
 app.post('/hook', (req, res)=>{
     console.log(req.body);
