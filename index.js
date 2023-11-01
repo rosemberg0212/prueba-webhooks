73a3063b515ada6a6748aa028f5288e55839254e
rous <rous@gmail.com>
2023-11-01 11:47:08 -0500

    primer seguimiento

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+app.use(express.json())
+const PORT = 3000;
+
+app.listen(PORT, ()=>console.log(`servidor corriendo en puerto ${PORT}`))
+
+app.post('/hook', (req, res)=>{
+    console.log(req.body);
+    res.status(200).end();
+})
