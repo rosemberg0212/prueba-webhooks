e018323e1329970aa1835fcf1e1a6c33ded8cadf
rous <rous@gmail.com>
2023-11-01 12:50:03 -0500

    challenge

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const app = express();
 app.use(express.json());
 const PORT = 3000;
 
-app.listen(PORT, ()=>console.log(`servidor corriendo en puerto ${PORT}`));
+app.listen(PORT, () => console.log(`servidor corriendo en puerto ${PORT}`));
 
-app.post('/hook', (req, res)=>{
+app.post('/hook', (req, res) => {
+    const challenge = req.body.challenge;
+    res.send({ challenge });
+
     console.log(req.body);
     res.status(200).end();
 })
